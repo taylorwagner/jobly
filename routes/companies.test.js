eb8ef588dfa50ff0c1070ef69afb392e5db3f53a
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-11-28 20:51:30 -0600

    jobs info testing

--- routes/companies.test.js
+++ routes/companies.test.js
@@ -159,6 +159,11 @@ describe("GET /companies/:handle", function () {
         description: "Desc1",
         numEmployees: 1,
         logoUrl: "http://c1.img",
+        jobs: [
+          { id: testJobIds[0], title: "J1", equity: "0.1", salary: 1 },
+          { id: testJobIds[1], title: "J2", equity: "0.2", salary: 2 },
+          { id: testJobIds[2], title: "J3", equity: null, salary: 3 },
+        ],
       },
     });
   });
