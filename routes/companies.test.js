2cccb8fdb9ae98582ac8f0543bf105e48a25ca8f
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-11-28 20:50:41 -0600

    adminToken for post request and get filtering testing

--- routes/companies.test.js
+++ routes/companies.test.js
@@ -56,7 +56,7 @@ describe("POST /companies", function () {
           handle: "new",
           numEmployees: 10,
         })
-        .set("authorization", `Bearer ${u1Token}`);
+        .set("authorization", `Bearer ${adminToken}`);
     expect(resp.statusCode).toEqual(400);
   });
 
@@ -67,7 +67,7 @@ describe("POST /companies", function () {
           ...newCompany,
           logoUrl: "not-a-url",
         })
-        .set("authorization", `Bearer ${u1Token}`);
+        .set("authorization", `Bearer ${adminToken}`);
     expect(resp.statusCode).toEqual(400);
   });
 });
@@ -105,15 +105,45 @@ describe("GET /companies", function () {
     });
   });
 
-  test("fails: test next() handler", async function () {
-    // there's no normal failure event which will cause this route to fail ---
-    // thus making it hard to test that the error-handler works with it. This
-    // should cause an error, all right :)
-    await db.query("DROP TABLE companies CASCADE");
+  test("works: filtering", async function () {
     const resp = await request(app)
         .get("/companies")
-        .set("authorization", `Bearer ${u1Token}`);
-    expect(resp.statusCode).toEqual(500);
+        .query({ minEmployees: 3 });
+    expect(resp.body).toEqual({
+      companies: [
+        {
+          handle: "c3",
+          name: "C3",
+          description: "Desc3",
+          numEmployees: 3,
+          logoUrl: "http://c3.img",
+        },
+      ],
+    });
+  });
+
+  test("works: filtering on all filters", async function () {
+    const resp = await request(app)
+        .get("/companies")
+        .query({ minEmployees: 2, maxEmployees: 3, name: "3" });
+    expect(resp.body).toEqual({
+      companies: [
+        {
+          handle: "c3",
+          name: "C3",
+          description: "Desc3",
+          numEmployees: 3,
+          logoUrl: "http://c3.img",
+        },
+      ],
+    });
+  });
+
+  test("bad request if invalid filter key", async function () {
+    const resp = await request(app)
+        .get("/companies")
+        .query({ minEmployees: 2, nope: "nope" });
+    expect(resp.statusCode).toEqual(400);
   });
 });
 
